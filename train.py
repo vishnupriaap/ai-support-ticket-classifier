e600a5e2bdde6883cab9afcc8a1fb02e0fc13724
Vishnupriya AP <vishnupriyaap42@gmail.com>
2026-09-24 18:46:14 +0530

    Add TF-IDF text classification

--- train.py
+++ train.py
@@ -1,5 +1,24 @@
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
 
 df = pd.read_csv("data/tickets.csv")
 
-print(df)
+X = df["text"]
+y = df["category"]
+
+vectorizer = TfidfVectorizer()
+
+X_tfidf = vectorizer.fit_transform(X)
+
+model = LogisticRegression()
+
+model.fit(X_tfidf, y)
+
+new_ticket = ["Money was deducted but my order failed"]
+
+new_ticket_tfidf = vectorizer.transform(new_ticket)
+
+prediction = model.predict(new_ticket_tfidf)
+
+print("Prediction:", prediction)
